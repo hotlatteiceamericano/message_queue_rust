a979637e31c3578bc03e0f0d80b2a613ce9b81f2
hotlatteiceamericano <hotlatteiceamericano@gmail.com>
2026-01-19 14:31:23 +0800

    update ConsumerGroup::poll method to keep track of read offset per topic

--- src/client/consumer_group.rs
+++ src/client/consumer_group.rs
@@ -8,9 +8,9 @@ pub struct ConsumerGroup {
 }
 
 impl ConsumerGroup {
-    pub fn new(name: String) -> Self {
+    pub fn new(name: &str) -> Self {
         Self {
-            name,
+            name: name.to_string(),
             read_offset: HashMap::new(),
         }
     }
@@ -33,25 +33,78 @@ impl ConsumerGroup {
     pub fn poll(&mut self, topic_name: String) -> io::Result<Message> {
         let mut topic = Topic::load(topic_name)?;
         let offset = self.read_offset().get(&topic).unwrap();
-        topic.read(offset.clone())
+        let message = topic.read(offset)?;
+
+        self.read_offset
+            .entry(topic)
+            .and_modify(|v| *v += message.total_length() as u64);
+
+        Ok(message)
     }
 }
 
 #[cfg(test)]
 mod test {
-    use crate::{client::consumer_group::ConsumerGroup, topic::topic::Topic};
+    use rstest::{fixture, rstest};
 
-    #[test]
-    pub fn test_write_poll() {
-        let mut consumer_group = ConsumerGroup::new(String::from("test_consumer_group"));
-        let topic = Topic::new(String::from("consumer_group_testing_topic"));
-        topic.save().unwrap();
+    use crate::client::consumer_group::ConsumerGroup;
+    use crate::test_utils::TestTopic;
+
+    #[fixture]
+    fn test_topic() -> TestTopic {
+        use crate::test_utils::TestTopic;
+        TestTopic::new()
+    }
+
+    #[rstest]
+    pub fn test_write_poll(test_topic: TestTopic) {
+        let mut consumer_group = ConsumerGroup::new("test_consumer_group");
+        // needed to call save as ConsumerGroup::write finds the topic from local file
+        // todo: find the way to not call the save()
+        test_topic.topic.save().unwrap();
+
         consumer_group
-            .write(topic.name().to_string(), String::from("hello world!"))
+            .write(
+                test_topic.topic.name().to_string(),
+                String::from("hello world!"),
+            )
             .unwrap();
 
-        let message = consumer_group.poll(topic.name().to_string()).unwrap();
+        let message = consumer_group
+            .poll(test_topic.topic.name().to_string())
+            .unwrap();
 
         assert_eq!(message.content, "hello world!");
+    }
+
+    #[rstest]
+    pub fn test_read_offset(test_topic: TestTopic) {
+        let mut consumer_group = ConsumerGroup::new("test_consumer_group");
+        test_topic.topic.save().unwrap();
+
+        consumer_group
+            .write(
+                test_topic.topic.name().to_string(),
+                String::from("hello world 1"),
+            )
+            .unwrap();
+        consumer_group
+            .write(
+                test_topic.topic.name().to_string(),
+                String::from("hello world 2"),
+            )
+            .unwrap();
+
+        let first_message = consumer_group
+            .poll(test_topic.topic.name().to_string())
+            .unwrap();
+
+        assert_eq!(first_message.content, "hello world 1");
+
+        let second_message = consumer_group
+            .poll(test_topic.topic.name().to_string())
+            .unwrap();
+
+        assert_eq!(second_message.content, "hello world 2");
     }
 }
